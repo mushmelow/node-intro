b975b01932bcede77df474e1ff2f0836541461fe
Charles-Antoine Francisco <charlantfr@gmail.com>
2017-07-09 11:51:01 -0400

    API Calls

--- workshop.js
+++ workshop.js
@@ -1,4 +1,5 @@
-var request = require('request-promise');
+const request = require('request-promise');
+const apiKeys = require('./api-keys.js');
 
 // Euclidian distance between two points
 function getDistance(pos1, pos2) {
@@ -6,27 +7,95 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-  return request(/* ... */)
-  .then(
-    function(responce) {
-      // Parse as JSON
-      // Return object with lat and lng
-    }
-  )
+  const options = {
+    uri: "http://api.open-notify.org/iss-now.json",
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
+  return request(options)
+  .then(function(response) {
+    return {
+      lat: response.iss_position.latitude,
+      lng: response.iss_position.longitude
+    };
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 }
 
 function getAddressPosition(address) {
-
+  const options = {
+    uri: "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + apiKeys.GOOGLE_MAPS,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
+  return request(options)
+  .then(function(response) {
+    return response.results[0].geometry.location;
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 }
 
 function getCurrentTemperatureAtPosition(position) {
-
+  const options = {
+    uri: "https://api.darksky.net/forecast/" + apiKeys.DARK_SKY + "/" + position.lat + "," + position.lng,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
+  return request(options)
+  .then(function(response) {
+    return response.currently.temperature;
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 }
 
 function getCurrentTemperature(address) {
-
+  return getAddressPosition(address)
+  .then(function(position) {
+    return getCurrentTemperatureAtPosition(position)
+  });
 }
 
 function getDistanceFromIss(address) {
-
+  return Promise.all(
+  [
+    getIssPosition(),
+    getAddressPosition(addressGoogle)
+  ])
+  .then(values => {
+    return getDistance(values[0], values[1]);
+  }).catch(err => {
+    console.log(err);
+  });
 }
+
+const addressGoogle = "1600+Amphitheatre+Parkway,+Mountain+View,+CA";
+
+getIssPosition().then(function(response) {
+  console.log("International Space Station Position :");
+  console.log(response);
+});
+
+getAddressPosition(addressGoogle).then(function(response) {
+  console.log("Google Headquarters Position : ");
+  console.log(response);
+});
+
+getCurrentTemperature(addressGoogle).then(function(temperature) {
+  console.log("Temperature at Google Headquarters : " + temperature);
+});
+
+getDistanceFromIss(addressGoogle).then(function(distance) {
+  console.log("Distance between ISS and Google = " + distance);
+});
