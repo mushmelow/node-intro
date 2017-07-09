50e14ea843c7ca979f274b71b611a066a4ca4c37
Hao zheng <zhenghao22@gmail.com>
2017-07-09 13:46:51 +0000

    getIssPosition

--- workshop.js
+++ workshop.js
@@ -1,4 +1,4 @@
-var request = require('request-promise');
+const request = require('request-promise');
 
 // Euclidian distance between two points
 function getDistance(pos1, pos2) {
@@ -6,16 +6,56 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-  return request(/* ... */)
+  const option= {
+    uri: "http://api.open-notify.org/iss-now.json",
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json:true
+  };
+  
+  return request(option)
   .then(
-    function(responce) {
+    function(response) {
+      console.log(option);
+      
+      
+      return {
+        lat:response.iss_position.latitude,
+        lng:response.iss_position.longitude
+      };
       // Parse as JSON
       // Return object with lat and lng
     }
   )
+  .catch(function (err) {
+    console.log(err, "the rejected error")
+        // POST failed...
+    });
 }
 
+getIssPosition().then(function (response){
+  console.log(response);
+});
+
 function getAddressPosition(address) {
+  
+   const option= {
+    uri: "https://developers.google.com/maps/documentation/geocoding/get-api-key",
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json:true
+  };
+  
+  
+
+  
+
+
+// google key :AIzaSyD5N8zyExKpsBXEzbJrWTCMEsZThUdhPto
+
+
 
 }
 
