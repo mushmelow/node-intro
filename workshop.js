742d989e867b194f09a79e10100d3be891215060
Hao zheng <zhenghao22@gmail.com>
2017-07-10 13:57:07 +0000

    node final

--- workshop.js
+++ workshop.js
@@ -23,9 +23,10 @@ function getIssPosition() {
       return {
         lat:response.iss_position.latitude,
         lng:response.iss_position.longitude
-      };
-      // Parse as JSON
+         // Parse as JSON
       // Return object with lat and lng
+      };
+     
     }
   )
   .catch(function (err) {
@@ -34,39 +35,120 @@ function getIssPosition() {
     });
 }
 
-getIssPosition().then(function (response){
-  console.log(response);
-});
+
+
 
 function getAddressPosition(address) {
   
+   const API_KEY= "AIzaSyD5N8zyExKpsBXEzbJrWTCMEsZThUdhPto";
+  
    const option= {
-    uri: "https://developers.google.com/maps/documentation/geocoding/get-api-key",
+    uri: "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key" + API_KEY, 
     headers: {
       'User-Agent': 'Request-Promise'
     },
     json:true
   };
   
-  
 
-  
-
-
-// google key :AIzaSyD5N8zyExKpsBXEzbJrWTCMEsZThUdhPto
-
-
+    return request(option)
+  .then(
+    
+    function(response) {
+    return response.results[0].geometry.location;
+    })
+      
+    .catch(function (err) {
+    console.log(err, "the rejected error");
+        // POST failed...
+    });
 
 }
 
 function getCurrentTemperatureAtPosition(position) {
+  const API_KEY="38ff65044045c0ef150e1a118de60f9c";
+  
+  
+  
+   const option= {
+    uri: "https://api.darksky.net/forecast/" + API_KEY + "/"+ position.lat+ ","+ position.lng, 
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json:true
+  };
+  
+
+    return request(option)
+  .then(
+    
+    function(response) {
+      
+    return response.currently.temperature;
+      
+    
+    })
+      
+    .catch(function (err) {
+    console.log(err, "the rejected error");
+        // POST failed...
+    });
 
 }
 
+  
+  
+
+
+
 function getCurrentTemperature(address) {
+  
+    return getAddressPosition(address).then(function(response) {
+     
+     return getCurrentTemperatureAtPosition(response);
+  })
+  
+  
 
 }
 
 function getDistanceFromIss(address) {
-
+  
+  
+  return Promise.all(
+    [getIssPosition(), getAddressPosition(address)]
+    ).then( responses => {
+      
+ 
+      
+      return getDistance(responses[0],responses[1]);
+    })
+    
+  
 }
+
+
+
+// getIssPosition().then(function (response){
+//   console.log(response);
+// });
+
+
+
+// getAddressPosition("1600+Amphitheatre+Parkway,+Mountain+View,+CA").then(function (response){
+//   console.log(response);
+// });
+
+// getCurrentTemperatureAtPosition({lat:37.8267,lng:-122.4233}).then(function (response){
+//   console.log(response);
+// });
+
+
+// getCurrentTemperature("1600+Amphitheatre+Parkway,+Mountain+View,+CA").then(function 
+// (response){
+//   console.log(response);
+// })
+
+getDistanceFromIss("1600+Amphitheatre+Parkway,+Mountain+View,+CA").then(function (response){
+  console.log(response);
+   });
